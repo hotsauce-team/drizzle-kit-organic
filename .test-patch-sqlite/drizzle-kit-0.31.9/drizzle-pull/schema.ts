5569550c55b27bdb03d4c70aea4a0af4335e6213
earthlingdavey <15802017+earthlingdavey@users.noreply.github.com>
2026-03-02 21:11:39 +0000

    chore: Remove test artifacts, add to gitignore

--- .test-patch-sqlite/drizzle-kit-0.31.9/drizzle-pull/schema.ts
+++
@@ -1,8 +0,0 @@
-import { sqliteTable, AnySQLiteColumn, integer, text } from "drizzle-orm/sqlite-core"
-  import { sql } from "drizzle-orm"
-
-export const users = sqliteTable("users", {
-	id: integer().primaryKey().notNull(),
-	name: text().notNull(),
-});
-
